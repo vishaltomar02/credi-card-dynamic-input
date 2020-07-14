f517498229f60303e887fe0d992d99483b8af688
Vishufame <vishal.tomar@famepilot.com>
2020-07-14 15:24:00 +0530

    task-done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import CreditCardInput from './CreditCardInput';
 
 function App() {
+
+  const [values, setValues] = useState({ no_of_inputs: '', no_of_digits: '' });
+  const [show, setShow] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    const errors = {};
+    Object.keys(values).forEach((item) => {
+      if (values[item] > 10) {
+        errors[item] = 'Please enter a value less than 10';
+      }
+    });
+    setErrors(errors);
+
+  }, [values]);
+
+  const handleSubmit = () => {
+    setShow(!show);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!show ?
+        <div>
+          <header >Choose the no. of inputs and no digits you want in each input</header>
+          <div className="main-inputs">
+            <section>
+              <label htmlFor="no_of_inputs">Enter No of Inputs</label>
+              <input id="no_of_inputs" type="number" name="no_of_inputs" onChange={(e) => setValues({ ...values, no_of_inputs: e.target.value })}></input>
+              <div className="error">{errors.no_of_inputs && errors.no_of_inputs}</div>
+            </section>
+            <section>
+              <label htmlFor="no_of_digits">Enter No of Digits</label>
+              <input id="no_of_digits" type="number" name="no_of_digits" onChange={(e) => setValues({ ...values, no_of_digits: e.target.value })}></input>
+              <div className="error">{errors.no_of_digits && errors.no_of_digits}</div>
+            </section>
+          </div>
+          <button className="button" type="text" onClick={handleSubmit}>Show</button>
+        </div> :
+        <CreditCardInput
+          noOfDigits={parseInt(values.no_of_digits)}
+          noOfInputs={parseInt(values.no_of_inputs)}
+          handleContentChange={handleSubmit}/>
+        }
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/CreditCardInput.js
@@ -0,0 +1,94 @@
+import React, { useState, useCallback, useRef, useEffect } from 'react';
+
+function CreditCardInput(props) {
+  const { noOfDigits: digits, noOfInputs: inputs } = props;
+  const [inputArray, setInputArray] = useState([]);
+  const [inputValues, setInputValues] = useState(new Array(inputs));
+
+  const inputRefs = useRef([]);
+
+  useEffect(() => {
+    const newInputArray = [];
+    for(let i = 0; i<inputs; i++)
+    {
+      newInputArray.push({id: i+1});
+    }
+    setInputArray(newInputArray);
+  },[inputs, setInputArray]);
+
+  function handleChange(e) {
+    const newInputValues = [...inputValues];
+    if (parseInt(e.target.id) === inputs && e.target.value.length >= digits) newInputValues[e.target.id - 1] = e.target.value.slice(0, digits);
+    else { newInputValues[e.target.id - 1] = e.target.value; }
+    setInputValues([...newInputValues]);
+  };
+
+  function handleKeyDown(e) {
+    const index = Math.floor(inputValues.join('').length / digits - 1);
+    const elemToFocus = inputRefs.current[index];
+    if (e.keyCode === 8 && (index) >= 0 && inputRefs.current[index + 1]) {
+      if (elemToFocus.value.length && !inputRefs.current[index + 1].value) {
+        elemToFocus.focus();
+        inputRefs.current[index + 1].blur();
+      }
+    }
+  }
+
+  const callBackRef = useCallback((index) => {
+    const currentElem = inputRefs.current[index];
+    if (currentElem && currentElem.value && currentElem.value.length > digits - 1) {
+      currentElem.setAttribute("value", currentElem.value.slice(0, digits));
+      inputRefs.current[index + 1] && inputRefs.current[index + 1].focus();
+    }
+  }, [digits]);
+
+
+  useEffect(() => {
+    inputValues.forEach((value, index) => {
+      if (!inputValues[index - 1] && value && value.length > digits) {
+        const newInputValues = inputValues.map((item, index) => {
+          const slicedValue = value.slice(index * digits, (index + 1) * digits);
+          if (slicedValue) {
+            return slicedValue;
+          }
+          else return item;
+        });
+        setInputValues(newInputValues);
+      }
+    });
+  }, [inputValues, digits]);
+
+  useEffect(() => {
+    const index = Math.floor(inputValues.join('').length / digits - 1);
+    callBackRef(index);
+  }, [callBackRef, inputValues, digits]);
+
+  return (
+    <div className="container">
+      <button className="button" type="text" onClick={props.handleContentChange}>Reset</button>
+      <h2>Enter you Card Number</h2>
+      <div className="card-input">
+        <h3>Card Number:</h3>
+        {/* <input id="hidden-input" ></input> */}
+        <div>
+          { inputArray && 
+            inputArray.map((item, index) => {
+              return (
+                <input
+                  key={index}
+                  ref={(element => inputRefs.current[index] = element)}
+                  id={item.id}
+                  type="number"
+                  value={inputValues[index]}
+                  onKeyDown={(e) => handleKeyDown(e)}
+                  onChange={(e) => handleChange(e)}></input>
+              );
+            })
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreditCardInput;
